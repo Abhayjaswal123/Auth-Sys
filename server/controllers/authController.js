7a40749adb845403d80ae9ae74e1dc2bd1a6bde4
Abhayjaswal <abhayjaswal312@gmail.com>
2025-12-11 19:08:04 +0530

    Fix: Set NODE_ENV to production and fix cookie sameSite casing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -101,7 +101,7 @@ export const logout = (req, res) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
         })
 
         return res.json({ success: true, message: "Logged Out" });
